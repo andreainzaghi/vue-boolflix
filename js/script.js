6812fa80446b2e6e0f04ffe80b3a3c45d84e9322
Andrea <andrea3aprile99@gmail.com>
2021-04-13 15:56:17 +0200

    d

--- js/script.js
+++ js/script.js
@@ -5,11 +5,13 @@ var app = new Vue({
     userQuery: "",
     movies: [],
     languages : ["it-IT","en-US"],
-  
+    textInput:"input-text",
+
 
   },
   methods: {
-    moviesSearch: function() {
+
+    moviesSearch: function(event) {
       axios.get('https://api.themoviedb.org/3/search/movie', {
         params: {
           api_key: "49659ac56a41879716b29b1fe3c048a3",
@@ -18,15 +20,39 @@ var app = new Vue({
           include_adult: false,
           selectlangindex:0,
           language: this.languages[this.selectlangindex]
-
         }
       })
       .then((response)=>{
         this.movies =   response.data.results;
         this.movieRate();
       });
+
       this.userQuery="";
+      axios.get('https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=scrubs', {
+        params: {
+
+          query: this.userQuery,
+          page: 1,
+          include_adult: false,
+          selectlangindex:0,
+          language: this.languages[this.selectlangindex]
+        }
+      })
+      .then((response)=>{
+        this.movies =   response.data.results;
+        this.movieRate();
+      });
+
     },
+    inputAnimation:function(event){
+  if (this.textInput=='input-text') {
+     this.textInput='input-text2';
+  }else{
+    this.textInput='input-text';
+  }
+
+},
+
     movieRate: function() {
       this.movies.forEach((item) => {
 
